935cb650034824afbe923bb40a418ca2626a4023
amradeland <amradeland@gylgroup.com>
2025-06-03 15:49:00 -0300

    first conection with the apiWeather

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+import styles from './Alert.module.css'
+
+type AlertProps = {
+    children: ReactNode
+}
+export default function Alert({children} : AlertProps) {
+  return (
+    <div className={styles.alert}>{children}</div>
+  )
+}
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import { countries } from "../../data/countries";
 import type { SearchType } from "../../types";
 import styles from "../Form/Form.module.css";
-export default function Form() {
+import Alert from "../Alert/Alert";
+
+type FormProps = {
+  fetchWeather: (search: SearchType) => Promise<void>;
+}
+
+export default function Form({fetchWeather}: FormProps) {
   const [search, setSearch] = useState<SearchType>({
     city: "",
     country: "",
   });
+
+  const [alert, setAlert] = useState('');
 
   const handleChange = (e:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>)=> {
     setSearch({
@@ -14,8 +22,21 @@ export default function Form() {
         [e.target.name] : e.target.value
     })
   }
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault();
+    if(Object.values(search).includes('')){
+      setAlert('Todos los campos son obligatorios');
+      return
+    }
+    fetchWeather(search);
+  }
   return (
-    <form className={styles.form}>
+    <form 
+    className={styles.form}
+    onSubmit={handleSubmit}
+    >
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad</label>
         <input
@@ -30,9 +51,9 @@ export default function Form() {
       <div className={styles.field}>
         <label htmlFor="country">Pais</label>
         <select id="country" value={search.country} name="country" onChange={handleChange}>
-          <option>--Seleccione un Pais--</option>
+          <option value="" id="">--Seleccione un Pais--</option>
           {countries.map((country) => (
-            <option value={country.code} key={country.code}>
+            <option value={country.code} key={country.code} className="every-country">
               {country.name}
             </option>
           ))}
